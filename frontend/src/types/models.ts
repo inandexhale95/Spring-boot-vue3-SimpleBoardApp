c8e2808bded3505e337ea784814d367e0e54da22
inandexhale95 <inandexhale95@gmail.com>
2024-07-25 22:44:25 +0900

    PageResultDTO 인터페이스 추가

--- frontend/src/types/models.ts
+++ frontend/src/types/models.ts
@@ -1,3 +1,20 @@
+export interface PageResultDTO {
+  dtoList: BoardAndReplyCount[];
+
+  totalPageCount: number;
+
+  currentPage: number;
+  size: number;
+
+  start: number;
+  end: number;
+
+  prev: boolean;
+  next: boolean;
+
+  pageList: number[];
+}
+
 export interface BoardAndReplyCount {
   email: string;
   nickname: string;
